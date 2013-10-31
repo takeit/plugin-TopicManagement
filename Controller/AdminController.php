da3177e79b91c1f8cde12df270deb558d3ae2486
Rafał Muszyński <rmuszynski1@gmail.com>
2013-10-31 09:23:59 +0100

    bundle set up

---
+++ Controller/AdminController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package Newscoop\CommentListsBundle
+ * @author Rafał Muszyński <rafal.muszynski@sourcefabric.org>
+ * @copyright 2013 Sourcefabric o.p.s.
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+namespace Newscoop\TopicManagementPluginBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin/topic-management")
+     * @Template()
+     */
+    public function indexAction(Request $request)
+    {
+        return array();
+    }
+}
---
+++ NewscoopTopicManagementPluginBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Newscoop\TopicManagementPluginBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NewscoopTopicManagementPluginBundle extends Bundle
+{
+}
